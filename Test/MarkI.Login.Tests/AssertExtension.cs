d0c823cff25fb06ff977328c6963459563f992a5
Slin castro <scastro@thoughtworks.com>
2018-06-25 20:18:39 -0500

    Setting internal Extension Assert Class

--- Test/MarkI.Login.Tests/AssertExtension.cs
+++ Test/MarkI.Login.Tests/AssertExtension.cs
@@ -3,9 +3,9 @@ using Xunit;
 
 namespace MarkI.Login.Tests
 {
-    public static  class AssertExtension
+    internal static  class AssertExtension
     {
-        public static  void WithMessage(this ArgumentException exception ,string expectedMessage)
+        internal static  void WithMessage(this ArgumentException exception ,string expectedMessage)
         {
            Assert.True(exception.Message.Equals(expectedMessage),userMessage:$"Expected message '{expectedMessage}'");
         }
