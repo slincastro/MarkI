4b6d19b89f8e316989036527d926d6e14c435e9f
Slin castro <scastro@thoughtworks.com>
2018-06-26 18:37:47 -0500

    refactoring test method

--- Test/MarkI.Departments.Tests/DepartmentService.Test.cs
+++ Test/MarkI.Departments.Tests/DepartmentService.Test.cs
@@ -67,7 +67,7 @@ namespace MarkI.Departments.Tests
 
         }
 
-         [Fact]
+        [Fact]
         public void ShouldCallRepositoryWithSameDepartmentWhenIAddNewDepartment()
         {
             var mockRepository = new Mock<IDepartments>();
