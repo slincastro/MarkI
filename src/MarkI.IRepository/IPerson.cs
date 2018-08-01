a7fd43e7d1cd36cb54ee4358264cf31a8093c03e
Slin castro <scastro@thoughtworks.com>
2018-07-31 20:49:53 -0500

    Create person service & Tests

---
+++ src/MarkI.IRepository/IPerson.cs
@@ -0,0 +1,6 @@
+using MarkI.Domain;
+
+namespace MarkI.IRepository
+{
+    public interface IPerson : IRepositoryBase<Person> {}
+}
---
+++ src/MarkI.Services/PersonService.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using MarkI.Domain;
+using MarkI.IRepository;
+
+namespace MarkI.Services
+{
+    public class PersonService
+    {
+        private IPerson _repository;
+
+        public PersonService(IPerson repository)
+        {
+            this._repository = repository;
+        }
+
+        public void Add(Person person)
+        {
+            _repository.Add(person);
+            
+        }
+
+        public List<Person> Get()
+        {
+            return _repository.Get();
+        }
+
+        public Person GetById(string validDocumentNumber)
+        {
+            return _repository.GetById(validDocumentNumber);
+        }
+    }
+}
